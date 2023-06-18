d5d0efa30556bbc61015eb476968923dcc7b9d6b
STMThundeR <91930458+STMThundeR@users.noreply.github.com>
2023-06-18 07:55:02 +0700

    Update Q8.sql

---
+++ Labs/Lab4/Q8.sql
@@ -0,0 +1,17 @@
+USE FUH_COMPANY
+GO 
+
+/*
+SELECT *
+FROM tblDependent
+GO
+
+SELECT *
+FROM tblEmployee
+GO
+*/
+
+SELECT depName, depBirthdate, empName
+FROM tblDependent d join tblEmployee e ON d.empSSN = e.empSSN
+WHERE  1990 /*năm tự chọn*/ - year(depBirthdate) > 18
+GO
